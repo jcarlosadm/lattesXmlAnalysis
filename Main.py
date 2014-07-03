515477dbb7f36e4e60f5ef1cea4c294af94d5d09
carlos <carlosmaster10@gmail.com>
2014-07-03 11:25:39 -0300

    alterando tabulacao

--- Main.py
+++ Main.py
@@ -3,61 +3,61 @@ import Pesquisadores # recebe arquivo fonte Pesquisadores.py
 import Relatorios # recebe arquivo fonte Relatorios.py
 
 #=============================================================
-#	*	Classe Analise_Xml
+#    *    Classe Analise_Xml
 #-------------------------------------------------------------
-#	Classe principal, recebe todas as outras classes
+#    Classe principal, recebe todas as outras classes
 #
-#	Objetos:
-#	pesquisadores	:	lista todos os pesquisadores
-#	relatorios		:	lista todos os relatorios
+#    Objetos:
+#    pesquisadores    :    lista todos os pesquisadores
+#    relatorios        :    lista todos os relatorios
 #
-#	Métodos:
-#	executar		:	executa o programa principal
+#    Métodos:
+#    executar        :    executa o programa principal
 #=============================================================
 
 class Analise_Xmls(object):
 
-	#---------------------------------------------------------
-	#	Inicialização
-	#---------------------------------------------------------
-	
-	def __init__(self):
-		self.ano_inicial = 0
-		self.ano_final = 0
-		self.configurar_ano()
-		self.pesquisadores = Pesquisadores.Pesquisadores()
-		self.relatorios = Relatorios.Relatorios(self.ano_inicial,self.ano_final)
-	
-	#---------------------------------------------------------
-	#	configura ano inicial e final
-	#---------------------------------------------------------
-	
-	def configurar_ano(self):
-		# abre arquivo de configuração
-		arquivo = open('init.txt','r')
-		# faz busca das configurações no arquivo aberto
-		linha = arquivo.readline()
-		while(linha!=''):
-			# se a linha for referente ao ano inicial das publicações, atribui à constante relacionada
-			if('ano inicial das publicacoes' in linha):
-				self.ano_inicial = int(linha.split(' : ')[1])
-			# se a linha for referente ao ano final das publicações, atribui à constante relacionada
-			elif('ano final das publicacoes' in linha):
-				self.ano_final = int(linha.split(' : ')[1])
-			linha = arquivo.readline()
-		# fecha arquivo de configuração
-		arquivo.close()
-	
-	#---------------------------------------------------------
-	#	Executa o programa principal
-	#---------------------------------------------------------
-	
-	def update(self):
-		self.pesquisadores.update(self.ano_inicial,self.ano_final,self.relatorios)
-		self.relatorios.update(self.ano_inicial,self.ano_final,self.pesquisadores)
+    #---------------------------------------------------------
+    #    Inicialização
+    #---------------------------------------------------------
+    
+    def __init__(self):
+        self.ano_inicial = 0
+        self.ano_final = 0
+        self.configurar_ano()
+        self.pesquisadores = Pesquisadores.Pesquisadores()
+        self.relatorios = Relatorios.Relatorios(self.ano_inicial,self.ano_final)
+    
+    #---------------------------------------------------------
+    #    configura ano inicial e final
+    #---------------------------------------------------------
+    
+    def configurar_ano(self):
+        # abre arquivo de configuração
+        arquivo = open('init.txt','r')
+        # faz busca das configurações no arquivo aberto
+        linha = arquivo.readline()
+        while(linha!=''):
+            # se a linha for referente ao ano inicial das publicações, atribui à constante relacionada
+            if('ano inicial das publicacoes' in linha):
+                self.ano_inicial = int(linha.split(' : ')[1])
+            # se a linha for referente ao ano final das publicações, atribui à constante relacionada
+            elif('ano final das publicacoes' in linha):
+                self.ano_final = int(linha.split(' : ')[1])
+            linha = arquivo.readline()
+        # fecha arquivo de configuração
+        arquivo.close()
+    
+    #---------------------------------------------------------
+    #    Executa o programa principal
+    #---------------------------------------------------------
+    
+    def update(self):
+        self.pesquisadores.update(self.ano_inicial,self.ano_final,self.relatorios)
+        self.relatorios.update(self.ano_inicial,self.ano_final,self.pesquisadores)
 
 #=============================================================
-#	Execução principal
+#    Execução principal
 #=============================================================
 
 programa = Analise_Xmls();
--- Pesquisador.py
+++ Pesquisador.py
@@ -1,118 +1,118 @@
 # -*- coding: UTF-8 -*-
 
 #=============================================================
-#	*	Classe Pesquisador
+#    *    Classe Pesquisador
 #-------------------------------------------------------------
-#	Classe que armazena dados dos pesquisadores
+#    Classe que armazena dados dos pesquisadores
 #
-#	Propriedades:
+#    Propriedades:
 #=============================================================
 
 class Pesquisador(object):
-	def __init__(self,nome,arquivoXML,pesquisadores,relatorios,ano_inicial,ano_final):
-		self.nome = nome
-		self.publicacoes = {"Artigos_completos_publicados_em_periodicos":{},"Livros_e_capitulos_de_livros_publicados":{},"Textos_em_jornais_e_noticias_revistas":{},'Trabalhos_completos_publicados_em_anais_de_congresso':{},'Apresentacoes_de_trabalhos':{},'Resumos_publicados_em_anais_de_congresso':{},'Acessorias_consultorias_e_outros_trabalhos_tecnicos':{},'Outras_producoes_bibliograficas':{}}
-		
-		self.inicializar_publicacoes(ano_inicial,ano_final)
-		
-		# artigos completos publicados em periodicos do pesquisador
-		self.processar_producao(ano_inicial,ano_final,arquivoXML,pesquisadores,relatorios,
-									'DADOS-BASICOS-DO-ARTIGO','NATUREZA','COMPLETO',
-									'ANO-DO-ARTIGO','TITULO-DO-ARTIGO','Artigos_completos_publicados_em_periodicos')
-		# livros e capitulos de livros publicados
-		self.processar_producao(ano_inicial,ano_final,arquivoXML,pesquisadores,relatorios,
-									'DADOS-BASICOS-DO-LIVRO','TIPO','LIVRO-PUBLICADO',
-									'ANO','TITULO-DO-LIVRO','Livros_e_capitulos_de_livros_publicados')
-		# capítulos de livros publicados
-		self.processar_producao(ano_inicial,ano_final,arquivoXML,pesquisadores,relatorios,
-									'DADOS-BASICOS-DO-CAPITULO','TIPO','Capítulo de livro publicado',
-									'ANO','TITULO-DO-CAPITULO-DO-LIVRO','Livros_e_capitulos_de_livros_publicados')
-		# textos em revistas
-		self.processar_producao(ano_inicial,ano_final,arquivoXML,pesquisadores,relatorios,
-									'DADOS-BASICOS-DO-TEXTO','NATUREZA','REVISTA_MAGAZINE',
-									'ANO-DO-TEXTO','TITULO-DO-TEXTO','Textos_em_jornais_e_noticias_revistas')
-		# textos em jornais
-		self.processar_producao(ano_inicial,ano_final,arquivoXML,pesquisadores,relatorios,
-									'DADOS-BASICOS-DO-TEXTO','NATUREZA','JORNAL_DE_NOTICIAS',
-									'ANO-DO-TEXTO','TITULO-DO-TEXTO','Textos_em_jornais_e_noticias_revistas')
-		# trabalhos completos publicados em congresso
-		self.processar_producao(ano_inicial,ano_final,arquivoXML,pesquisadores,relatorios,
-									'DADOS-BASICOS-DO-TRABALHO',"NATUREZA",'COMPLETO',
-									'ANO-DO-TRABALHO','TITULO-DO-TRABALHO','Trabalhos_completos_publicados_em_anais_de_congresso')
-		# apresentações de trabalho em congressos
-		self.processar_producao(ano_inicial,ano_final,arquivoXML,pesquisadores,relatorios,
-									'DADOS-BASICOS-DA-APRESENTACAO-DE-TRABALHO',"","",
-									'ANO','TITULO','Apresentacoes_de_trabalhos')
-		# resumos expandidos publicados em anais de congressos
-		self.processar_producao(ano_inicial,ano_final,arquivoXML,pesquisadores,relatorios,
-									'DADOS-BASICOS-DO-TRABALHO','NATUREZA','RESUMO_EXPANDIDO',
-									'ANO-DO-TRABALHO','TITULO-DO-TRABALHO','Resumos_publicados_em_anais_de_congresso')
-		# resumos publicados em anais de congresso
-		self.processar_producao(ano_inicial,ano_final,arquivoXML,pesquisadores,relatorios,
-									'DADOS-BASICOS-DO-TRABALHO','NATUREZA','RESUMO',
-									'ANO-DO-TRABALHO','TITULO-DO-TRABALHO','Resumos_publicados_em_anais_de_congresso')
-		# resumos publicados em anais de congresso (artigos)
-		self.processar_producao(ano_inicial,ano_final,arquivoXML,pesquisadores,relatorios,
-									'DADOS-BASICOS-DO-ARTIGO','NATUREZA','RESUMO',
-									'ANO-DO-ARTIGO','TITULO-DO-ARTIGO','Resumos_publicados_em_anais_de_congresso')
-		# Acessorias, consultorias e trabalhos técnicos
-		self.processar_producao(ano_inicial,ano_final,arquivoXML,pesquisadores,relatorios,
-									'DADOS-BASICOS-DO-TRABALHO-TECNICO','','','ANO','TITULO-DO-TRABALHO-TECNICO'
-									,'Acessorias_consultorias_e_outros_trabalhos_tecnicos')
-		# Cursos de curta duração ministrados
-		self.processar_producao(ano_inicial,ano_final,arquivoXML,pesquisadores,relatorios,
-									'DADOS-BASICOS-DE-CURSOS-CURTA-DURACAO-MINISTRADO','','',
-									'ANO','TITULO','Acessorias_consultorias_e_outros_trabalhos_tecnicos')
-		# Outras produções técnicas
-		self.processar_producao(ano_inicial,ano_final,arquivoXML,pesquisadores,relatorios,
-									'DADOS-BASICOS-DE-OUTRA-PRODUCAO-TECNICA','','',
-									'ANO','TITULO','Acessorias_consultorias_e_outros_trabalhos_tecnicos')
-		# Demais trabalhos
-		self.processar_producao(ano_inicial,ano_final,arquivoXML,pesquisadores,relatorios,
-									'DADOS-BASICOS-DE-DEMAIS-TRABALHOS','','',
-									'ANO','TITULO','Acessorias_consultorias_e_outros_trabalhos_tecnicos')
-		# Outras produções bibliográficas
-		self.processar_producao(ano_inicial,ano_final,arquivoXML,pesquisadores,relatorios,
-									'DADOS-BASICOS-DE-OUTRA-PRODUCAO','','','ANO','TITULO','Outras_producoes_bibliograficas')
-		
-	def inicializar_publicacoes(self,ano_inicial,ano_final):
-		for ano in range(ano_inicial,ano_final+1):
-			# usar for key in self.publicações
-			self.publicacoes['Artigos_completos_publicados_em_periodicos'][ano] = []
-			self.publicacoes['Livros_e_capitulos_de_livros_publicados'][ano]=[]
-			self.publicacoes["Textos_em_jornais_e_noticias_revistas"][ano]=[]
-			self.publicacoes["Trabalhos_completos_publicados_em_anais_de_congresso"][ano]=[]
-			self.publicacoes['Apresentacoes_de_trabalhos'][ano]=[]
-			self.publicacoes['Resumos_publicados_em_anais_de_congresso'][ano]=[]
-			self.publicacoes['Acessorias_consultorias_e_outros_trabalhos_tecnicos'][ano]=[]
-			self.publicacoes['Outras_producoes_bibliograficas'][ano]=[]
-		
-	def processar_producao(self,ano_inicial,ano_final,arquivoXml,pesquisadores,relatorios,nome_tag,
-							atributo_filtro_item,valor_atributo_item,atributo_ano_item,atributo_titulo_item,
-							tipo_producao,filtro_adicional=False,tipo_filtro_adiconal=''):
+    def __init__(self,nome,arquivoXML,pesquisadores,relatorios,ano_inicial,ano_final):
+        self.nome = nome
+        self.publicacoes = {"Artigos_completos_publicados_em_periodicos":{},"Livros_e_capitulos_de_livros_publicados":{},"Textos_em_jornais_e_noticias_revistas":{},'Trabalhos_completos_publicados_em_anais_de_congresso':{},'Apresentacoes_de_trabalhos':{},'Resumos_publicados_em_anais_de_congresso':{},'Acessorias_consultorias_e_outros_trabalhos_tecnicos':{},'Outras_producoes_bibliograficas':{}}
+        
+        self.inicializar_publicacoes(ano_inicial,ano_final)
+        
+        # artigos completos publicados em periodicos do pesquisador
+        self.processar_producao(ano_inicial,ano_final,arquivoXML,pesquisadores,relatorios,
+                                    'DADOS-BASICOS-DO-ARTIGO','NATUREZA','COMPLETO',
+                                    'ANO-DO-ARTIGO','TITULO-DO-ARTIGO','Artigos_completos_publicados_em_periodicos')
+        # livros e capitulos de livros publicados
+        self.processar_producao(ano_inicial,ano_final,arquivoXML,pesquisadores,relatorios,
+                                    'DADOS-BASICOS-DO-LIVRO','TIPO','LIVRO-PUBLICADO',
+                                    'ANO','TITULO-DO-LIVRO','Livros_e_capitulos_de_livros_publicados')
+        # capítulos de livros publicados
+        self.processar_producao(ano_inicial,ano_final,arquivoXML,pesquisadores,relatorios,
+                                    'DADOS-BASICOS-DO-CAPITULO','TIPO','Capítulo de livro publicado',
+                                    'ANO','TITULO-DO-CAPITULO-DO-LIVRO','Livros_e_capitulos_de_livros_publicados')
+        # textos em revistas
+        self.processar_producao(ano_inicial,ano_final,arquivoXML,pesquisadores,relatorios,
+                                    'DADOS-BASICOS-DO-TEXTO','NATUREZA','REVISTA_MAGAZINE',
+                                    'ANO-DO-TEXTO','TITULO-DO-TEXTO','Textos_em_jornais_e_noticias_revistas')
+        # textos em jornais
+        self.processar_producao(ano_inicial,ano_final,arquivoXML,pesquisadores,relatorios,
+                                    'DADOS-BASICOS-DO-TEXTO','NATUREZA','JORNAL_DE_NOTICIAS',
+                                    'ANO-DO-TEXTO','TITULO-DO-TEXTO','Textos_em_jornais_e_noticias_revistas')
+        # trabalhos completos publicados em congresso
+        self.processar_producao(ano_inicial,ano_final,arquivoXML,pesquisadores,relatorios,
+                                    'DADOS-BASICOS-DO-TRABALHO',"NATUREZA",'COMPLETO',
+                                    'ANO-DO-TRABALHO','TITULO-DO-TRABALHO','Trabalhos_completos_publicados_em_anais_de_congresso')
+        # apresentações de trabalho em congressos
+        self.processar_producao(ano_inicial,ano_final,arquivoXML,pesquisadores,relatorios,
+                                    'DADOS-BASICOS-DA-APRESENTACAO-DE-TRABALHO',"","",
+                                    'ANO','TITULO','Apresentacoes_de_trabalhos')
+        # resumos expandidos publicados em anais de congressos
+        self.processar_producao(ano_inicial,ano_final,arquivoXML,pesquisadores,relatorios,
+                                    'DADOS-BASICOS-DO-TRABALHO','NATUREZA','RESUMO_EXPANDIDO',
+                                    'ANO-DO-TRABALHO','TITULO-DO-TRABALHO','Resumos_publicados_em_anais_de_congresso')
+        # resumos publicados em anais de congresso
+        self.processar_producao(ano_inicial,ano_final,arquivoXML,pesquisadores,relatorios,
+                                    'DADOS-BASICOS-DO-TRABALHO','NATUREZA','RESUMO',
+                                    'ANO-DO-TRABALHO','TITULO-DO-TRABALHO','Resumos_publicados_em_anais_de_congresso')
+        # resumos publicados em anais de congresso (artigos)
+        self.processar_producao(ano_inicial,ano_final,arquivoXML,pesquisadores,relatorios,
+                                    'DADOS-BASICOS-DO-ARTIGO','NATUREZA','RESUMO',
+                                    'ANO-DO-ARTIGO','TITULO-DO-ARTIGO','Resumos_publicados_em_anais_de_congresso')
+        # Acessorias, consultorias e trabalhos técnicos
+        self.processar_producao(ano_inicial,ano_final,arquivoXML,pesquisadores,relatorios,
+                                    'DADOS-BASICOS-DO-TRABALHO-TECNICO','','','ANO','TITULO-DO-TRABALHO-TECNICO'
+                                    ,'Acessorias_consultorias_e_outros_trabalhos_tecnicos')
+        # Cursos de curta duração ministrados
+        self.processar_producao(ano_inicial,ano_final,arquivoXML,pesquisadores,relatorios,
+                                    'DADOS-BASICOS-DE-CURSOS-CURTA-DURACAO-MINISTRADO','','',
+                                    'ANO','TITULO','Acessorias_consultorias_e_outros_trabalhos_tecnicos')
+        # Outras produções técnicas
+        self.processar_producao(ano_inicial,ano_final,arquivoXML,pesquisadores,relatorios,
+                                    'DADOS-BASICOS-DE-OUTRA-PRODUCAO-TECNICA','','',
+                                    'ANO','TITULO','Acessorias_consultorias_e_outros_trabalhos_tecnicos')
+        # Demais trabalhos
+        self.processar_producao(ano_inicial,ano_final,arquivoXML,pesquisadores,relatorios,
+                                    'DADOS-BASICOS-DE-DEMAIS-TRABALHOS','','',
+                                    'ANO','TITULO','Acessorias_consultorias_e_outros_trabalhos_tecnicos')
+        # Outras produções bibliográficas
+        self.processar_producao(ano_inicial,ano_final,arquivoXML,pesquisadores,relatorios,
+                                    'DADOS-BASICOS-DE-OUTRA-PRODUCAO','','','ANO','TITULO','Outras_producoes_bibliograficas')
+        
+    def inicializar_publicacoes(self,ano_inicial,ano_final):
+        for ano in range(ano_inicial,ano_final+1):
+            # usar for key in self.publicações
+            self.publicacoes['Artigos_completos_publicados_em_periodicos'][ano] = []
+            self.publicacoes['Livros_e_capitulos_de_livros_publicados'][ano]=[]
+            self.publicacoes["Textos_em_jornais_e_noticias_revistas"][ano]=[]
+            self.publicacoes["Trabalhos_completos_publicados_em_anais_de_congresso"][ano]=[]
+            self.publicacoes['Apresentacoes_de_trabalhos'][ano]=[]
+            self.publicacoes['Resumos_publicados_em_anais_de_congresso'][ano]=[]
+            self.publicacoes['Acessorias_consultorias_e_outros_trabalhos_tecnicos'][ano]=[]
+            self.publicacoes['Outras_producoes_bibliograficas'][ano]=[]
+        
+    def processar_producao(self,ano_inicial,ano_final,arquivoXml,pesquisadores,relatorios,nome_tag,
+                            atributo_filtro_item,valor_atributo_item,atributo_ano_item,atributo_titulo_item,
+                            tipo_producao,filtro_adicional=False,tipo_filtro_adiconal=''):
 
-		itens = arquivoXml.getElementsByTagName(nome_tag)
-		for ano in range(ano_inicial,ano_final+1):
-			for item in itens:
+        itens = arquivoXml.getElementsByTagName(nome_tag)
+        for ano in range(ano_inicial,ano_final+1):
+            for item in itens:
 
-				if(filtro_adicional):
-					item = item.getElementsByTagName(tipo_filtro_adiconal)[0]
+                if(filtro_adicional):
+                    item = item.getElementsByTagName(tipo_filtro_adiconal)[0]
 
-				if(atributo_filtro_item==''):
-					atributo = True
-				else:
-					atributo = (item.attributes[atributo_filtro_item].value.encode('iso8859-1')==valor_atributo_item)
+                if(atributo_filtro_item==''):
+                    atributo = True
+                else:
+                    atributo = (item.attributes[atributo_filtro_item].value.encode('iso8859-1')==valor_atributo_item)
 
-				if(atributo and item.attributes[atributo_ano_item].value==str(ano)):
-					if(self.producao_exclusiva(item.attributes[atributo_titulo_item].value,tipo_producao,ano,pesquisadores)):
-						relatorios.contagem[tipo_producao][ano]+=1
-					self.publicacoes[tipo_producao][ano].append(item.attributes[atributo_titulo_item].value)
+                if(atributo and item.attributes[atributo_ano_item].value==str(ano)):
+                    if(self.producao_exclusiva(item.attributes[atributo_titulo_item].value,tipo_producao,ano,pesquisadores)):
+                        relatorios.contagem[tipo_producao][ano]+=1
+                    self.publicacoes[tipo_producao][ano].append(item.attributes[atributo_titulo_item].value)
 
-			self.publicacoes[tipo_producao][ano].sort()
-			
-	def producao_exclusiva(self,titulo,tipo_producao,ano,pesquisadores):
-		for pesquisador in pesquisadores:
-			for publicacao in (pesquisador.publicacoes[tipo_producao][ano]):
-				if(titulo == publicacao):
-					return False # nao e exlusiva
-		return True # é exclusiva
+            self.publicacoes[tipo_producao][ano].sort()
+            
+    def producao_exclusiva(self,titulo,tipo_producao,ano,pesquisadores):
+        for pesquisador in pesquisadores:
+            for publicacao in (pesquisador.publicacoes[tipo_producao][ano]):
+                if(titulo == publicacao):
+                    return False # nao e exlusiva
+        return True # é exclusiva
